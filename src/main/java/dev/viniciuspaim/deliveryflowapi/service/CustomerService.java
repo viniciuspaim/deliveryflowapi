0b360e5fe04d6c86b93f9033ba773aa0466befea
viniciuspaim <viniciuspaimdacosta@yahoo.com.br>
2026-03-20 07:57:15 -0300

    fix: added a validation to verify if the customer exists

--- src/main/java/dev/viniciuspaim/deliveryflowapi/service/CustomerService.java
+++ src/main/java/dev/viniciuspaim/deliveryflowapi/service/CustomerService.java
@@ -35,6 +35,7 @@ public class CustomerService {
     }
 
     public void deleteById(Long id) {
-        customerRepository.deleteById(id);
+        Customer customer = findById(id);
+        customerRepository.delete(customer);
     }
 }
