88d26c06a7631e4ee9d2e78520ae958fec389372
viniciuspaim <viniciuspaimdacosta@yahoo.com.br>
2026-04-30 13:30:37 -0300

    test: add H2 in-memory database for test profile

--- src/main/java/dev/viniciuspaim/deliveryflowapi/config/FlywayConfig.java
+++ src/main/java/dev/viniciuspaim/deliveryflowapi/config/FlywayConfig.java
@@ -3,10 +3,12 @@ package dev.viniciuspaim.deliveryflowapi.config;
 import org.flywaydb.core.Flyway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
 @Configuration
+@Profile("!test")
 public class FlywayConfig {
 
     @Bean(initMethod = "migrate")
--- src/test/java/dev/viniciuspaim/deliveryflowapi/DeliveryFlowApiApplicationTests.java
+++ src/test/java/dev/viniciuspaim/deliveryflowapi/DeliveryFlowApiApplicationTests.java
@@ -2,8 +2,10 @@ package dev.viniciuspaim.deliveryflowapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DeliveryFlowApiApplicationTests {
 
     @Test
